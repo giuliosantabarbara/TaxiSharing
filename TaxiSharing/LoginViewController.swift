b18c0a5cea2221bf64ae6a0fb9b14a0240e5b4c2
DomenicoNatella <domenico.natella@gmail.com>
2017-10-09 16:59:09 +0200

    Login

---
+++ TaxiSharing/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  TaxiSharing
+//
+//  Created by Domenico Natella on 09/10/17.
+//  Copyright © 2017 Santabarbara Pietro Giulio. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIAlertController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
