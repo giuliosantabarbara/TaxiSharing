a6c1671ebc0c50b323a92e5ff449c232ec0bf4f6
DomenicoNatella <domenico.natella@gmail.com>
2017-10-09 17:38:01 +0200

    Login modified

--- TaxiSharing/LoginViewController.swift
+++ TaxiSharing/LoginViewController.swift
@@ -8,29 +8,66 @@
 
 import UIKit
 
-class LoginViewController: UIAlertController {
-
+class LoginViewController: UIViewController {
+    @IBOutlet weak var textFieldLoginEmail: UIButton!
+    @IBOutlet weak var textFieldLoginPassword: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        FIRAuth.auth()!.addStateDidChangeListener() { auth, user in
+            if user != nil {
+                self.performSegue(withIdentifier: self.loginToList, sender: nil)
+            }
+        }
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginDidTouch(_ sender: UIButton) {
+        FIRAuth.auth()!.signIn(withEmail: textFieldLoginEmail.text!,
+                               password: textFieldLoginPassword.text!)
     }
-    */
+    
+    @IBAction func signupDidTouch(_ sender: Any) {
+        let alert = UIAlertController(title: "Register",
+                                      message: "Register",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save",style: .default) { action in
+            let emailField = alert.textFields![0]
+            let passwordField = alert.textFields![1]
+            FIRAuth.auth()!.createUser(withEmail: emailField.text!,password: passwordField.text!) { user, error in
+                    if error == nil {
+                        FIRAuth.auth()!.signIn(withEmail: self.textFieldLoginEmail.text!, password: self.textFieldLoginPassword.text!)
+                    }
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        alert.addTextField { textEmail in textEmail.placeholder = "Enter your email"}
+        alert.addTextField { textPassword in
+            textPassword.isSecureTextEntry = true
+            textPassword.placeholder = "Enter your password"
+        }
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
 
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == textFieldLoginEmail {
+            textFieldLoginPassword.becomeFirstResponder()
+        }
+        if textField == textFieldLoginPassword {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
 }
